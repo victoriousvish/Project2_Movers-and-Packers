56e5d395e5b06493ee560a4ad8dad9a85049af57
victoriousvish <vishalpal959@gmail.com>
2022-01-27 20:21:37 +0530

    hi

---
+++ Website/home.js
@@ -0,0 +1,14 @@
+function changeMode(){
+    var mybody = document.body;
+     mybody.classList.toggle("mydark")
+}
+
+
+function test(){
+    document.getElementById('coupon').style.visibility="visible"
+}
+
+function closeDiv(){
+    document.getElementById('coupon').style.visibility="hidden"
+}
+
